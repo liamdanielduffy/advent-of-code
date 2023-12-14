9a4eb920aa69f0ca3ba46623d2c0ba51f3320b4d
Liam Duffy <liamdanielduffy@gmail.com>
2023-12-14 13:30:36 -0500

    cleanup day 1 answers

--- src/advent_of_code/core.clj
+++ src/advent_of_code/core.clj
@@ -1,5 +1,13 @@
 (ns advent-of-code.core
-  (:require [advent-of-code.day-1 :as day1]
+  (:require [advent-of-code.day-1 :as d1]
             [advent-of-code.puzzle-inputs :as inputs]))
 
-(def day1-part1-answer (day1/sum-calibration-values inputs/day1))
+(def day1-part1-answer (->> inputs/day1
+                            (d1/split-on-newlines)
+                            (map d1/get-calibration-value-pt1)
+                            (apply +)))
+
+(def day1-part2-answer (->> inputs/day1
+                            (d1/split-on-newlines)
+                            (map d1/get-calibration-value-pt2)
+                            (apply +)))
--- src/advent_of_code/day_1.clj
+++ src/advent_of_code/day_1.clj
@@ -1,16 +1,92 @@
 (ns advent-of-code.day-1
-  (:require [advent-of-code.puzzle-inputs :as inputs]))
+  (:require [advent-of-code.puzzle-inputs :as inputs]
+            [clojure.string :as s]))
+
+(def pt1-strings-to-numbers
+  {"1" 1
+   "2" 2
+   "3" 3
+   "4" 4
+   "5" 5
+   "6" 6
+   "7" 7
+   "8" 8
+   "9" 9})
+
+(def pt2-strings-to-numbers
+  {"one" 1
+   "1" 1
+   "two" 2
+   "2" 2
+   "three" 3
+   "3" 3
+   "four" 4
+   "4" 4
+   "five" 5
+   "5" 5
+   "six" 6
+   "6" 6
+   "seven" 7
+   "7" 7
+   "eight" 8
+   "8" 8
+   "nine" 9
+   "9" 9})
+
+;; get a vector of the input lines
 
 (defn split-on-newlines [s] (clojure.string/split s #"\n"))
 
-(defn remove-non-digits [s] (clojure.string/replace s #"[^\d]" ""))
+;; get substrings from each index in the string - 0,1,2,3...
 
-(defn get-calibration-value [s]
-  (Integer/parseInt (str (first s) (last s))))
+(defn substrings-after-indices [s indices]
+  (map #(subs s %) indices))
 
-(defn sum-calibration-values [s]
+(defn character-indices [s]
+  (range 0 (count s)))
+
+(defn substrings-from-start [s]
+  (substrings-after-indices s (character-indices s)))
+
+;; for each substring, figure out what number, if any, starts the string
+;; customize how numbers can be found by passing a strings-to-numbers array
+
+(defn get-number-starting-string [strings-to-numbers s]
+  (some (fn [[key value]]
+          (if (s/starts-with? s key)
+            value))
+        strings-to-numbers))
+
+(defn get-numbers-starting-strings [strings-to-numbers strings]
+  (map #(get-number-starting-string strings-to-numbers %) strings))
+
+;; get rid of nils, representing substrings with no numbers
+
+(defn remove-nils [numbers]
+  (filter identity numbers))
+
+;; get the first and last number, concatenate them to make the calibration value
+
+(defn convert-to-calibration-value [ints]
+  (let [string-ints (map #(str %) ints)
+        calibration-string (str (first string-ints) (last string-ints))]
+    (Integer/parseInt calibration-string)))
+
+;; get calibration value for a string, given a map of number-representations-in-strings to numbers
+
+(defn get-calibration-value [strings-to-numbers s]
   (->> s
-       (split-on-newlines)
-       (map remove-non-digits)
-       (map get-calibration-value)
-       (apply +)))
+       (substrings-from-start)
+       (get-numbers-starting-strings strings-to-numbers)
+       (remove-nils)
+       (convert-to-calibration-value)))
+
+;; shorthand for using the pt1 map from number-representations to numbers
+
+(defn get-calibration-value-pt1 [s]
+  (get-calibration-value pt1-strings-to-numbers s))
+
+;; shorthand for using the pt2 map from number-representations to numbers
+
+(defn get-calibration-value-pt2 [s]
+  (get-calibration-value pt2-strings-to-numbers s))
