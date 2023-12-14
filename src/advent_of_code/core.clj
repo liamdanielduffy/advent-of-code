64f03a64b35a27710140dd0dea4369d846309d90
Liam Duffy <liamdanielduffy@gmail.com>
2023-12-13 22:10:35 -0500

    finishes day 1 part 1

--- src/advent_of_code/core.clj
+++ src/advent_of_code/core.clj
@@ -1,3 +1,5 @@
-(ns advent-of-code.core)
+(ns advent-of-code.core
+  (:require [advent-of-code.day-1 :as day1]
+            [advent-of-code.puzzle-inputs :as inputs]))
 
-(defn hello [] "hello")
+(def day1-part1-answer (day1/sum-calibration-values inputs/day1))
--- src/advent_of_code/day_1.clj
+++ src/advent_of_code/day_1.clj
@@ -1,2 +1,16 @@
-(ns advent-of-code.day-1)
+(ns advent-of-code.day-1
+  (:require [advent-of-code.puzzle-inputs :as inputs]))
 
+(defn split-on-newlines [s] (clojure.string/split s #"\n"))
+
+(defn remove-non-digits [s] (clojure.string/replace s #"[^\d]" ""))
+
+(defn get-calibration-value [s]
+  (Integer/parseInt (str (first s) (last s))))
+
+(defn sum-calibration-values [s]
+  (->> s
+       (split-on-newlines)
+       (map remove-non-digits)
+       (map get-calibration-value)
+       (apply +)))
